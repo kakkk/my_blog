97d80e7c4f06009f1194f07613ee1d46eb3b6601
kakkk <kakkk@live.com>
2023-05-19 13:48:52 +0800

    feat: remove mocker

--- my_blog_server/biz/facade/page_search.go
+++ my_blog_server/biz/facade/page_search.go
@@ -4,12 +4,20 @@ import (
 	"context"
 	"net/http"
 
-	"my_blog/biz/mock"
+	"my_blog/biz/common/config"
 	"my_blog/biz/model/blog/page"
 
 	"github.com/cloudwego/hertz/pkg/app"
 )
 
 func SearchPage(ctx context.Context, c *app.RequestContext) (resp *page.BasicPageResp, code int) {
-	return mock.SearchMocker(), http.StatusOK
+	return &page.BasicPageResp{
+		Meta: &page.PageMeta{
+			Title:       "Search",
+			Description: "search",
+			SiteDomain:  config.GetSiteConfig().SiteDomain,
+			CDNDomain:   config.GetSiteConfig().CDNDomain,
+			PageType:    page.PageTypeSearch,
+		},
+	}, http.StatusOK
 }

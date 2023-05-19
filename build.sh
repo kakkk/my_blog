9956bc38f32cb106cbfadd1c714011dd6ebcc5db
kakkk <kakkk@live.com>
2023-05-19 18:17:50 +0800

    fix: katex fonts

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ rm -rf output
 mkdir -p output/bin
 mkdir -p output/assets/css
 mkdir -p output/assets/js
+mkdir -p output/assets/katex
 mkdir -p output/templates
 mkdir -p output/conf/
 mkdir -p output/templates/
@@ -11,9 +12,6 @@ cp script/* output/
 cp conf/* output/conf/
 cp my_blog_front/templates/* output/templates/
 cp my_blog_front/assets/css/stylesheet.min.css output/assets/css/stylesheet.min.css
-cp my_blog_front/assets/css/katex/katex.min.css output/assets/css/katex.min.css
 cp my_blog_front/assets/js/highlight.min.js output/assets/js/highlight.min.js
-cp my_blog_front/assets/js/katex.min.js output/assets/js/katex.min.js
-cp my_blog_front/assets/js/auto-render.min.js output/assets/js/auto-render.min.js
-cp my_blog_server/biz/mock/mock_post.md output/mock_post.md
+cp -r my_blog_front/assets/katex/* output/assets/katex/
 cd my_blog_server && go build -o ../output/bin/my_blog

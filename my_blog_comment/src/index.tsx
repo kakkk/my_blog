a656a91c2c9580ca950c936896a66770272bf4cb
kakkk <kakkk@live.com>
2023-08-17 13:52:18 +0800

    feat: basic ui

--- my_blog_comment/src/index.tsx
+++ my_blog_comment/src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const container = document.getElementById('comment');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);

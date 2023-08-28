2af528b321e7b8c94a0ce847a1ee765c3a260052
kakkk <kakkk@live.com>
2023-08-29 01:20:45 +0800

    build: support comment project

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+echo -e '🔨\033[32m build comment... \033[0m'
+cd my_blog_comment && yarn build
+cd ..
+echo -e '🔨\033[32m build pages... \033[0m'
 cd my_blog_pages && yarn build
 cd ..
+echo -e '🔨\033[32m build admin... \033[0m'
 cd my_blog_admin && yarn build
 cd ..
 rm -rf output
@@ -13,6 +18,9 @@ mkdir -p output/conf
 cp script/* output/
 cp conf/* output/conf/
 cp my_blog_pages/src/templates/* output/templates/
+cp my_blog_comment/dist/comment.min.js output/assets/comment.min.js
 cp my_blog_pages/dist/* output/assets/
 cp my_blog_admin/dist/* output/admin/
+echo -e '🔨\033[32m build server... \033[0m'
 cd my_blog_server && go build -o ../output/bin/my_blog
+echo -e '🔨\033[32m build success!!! \033[0m'
